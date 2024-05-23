5dd868d9617fe6a7b48350edab991178992a1009
Sarah <74245537+Sarenard@users.noreply.github.com>
2024-05-23 21:50:39 +0200

    automation of rust-project.json

--- tools/build.py
+++ tools/build.py
@@ -1,7 +1,44 @@
+import subprocess
+import json
 import sys
 
+def get_sysroot() -> str:
+    sysroot_cmd = [
+        "rustc",
+        "--print",
+        "sysroot",
+    ]
+    output = subprocess.Popen(
+        sysroot_cmd, stdout=subprocess.PIPE
+    ).communicate()[0]
+    sysroot = output.decode('utf-8')[:-1]
+    return sysroot
+
+def generate_json(sysroot):
+    json_struct = {
+        "sysroot" : sysroot,
+        "crates": [
+            {
+                "root_module" : "src/main.rs",
+                "edition" : "2021",
+                "deps" : [],
+                "cfg" : [],
+                "env" : {
+
+                },
+                "is_proc_macro" : False
+            }
+        ]
+    }
+    return json_struct
+
 def build():
-    ...
+    # we make the rust-project.json file first
+    sysroot = get_sysroot()
+    json_struct = generate_json(sysroot)
+    serialized = json.dumps(json_struct, sort_keys=True, indent=4)
+    with open("rust-project.json", "w") as f:
+        f.write(serialized)
 
 def help():
     ...
@@ -11,10 +48,12 @@ functions = {
     "help" : help,
 }
 
+# main function
 def main():
     if len(sys.argv) < 2:
         print("No arguments specified, please use the Makefile")
-        return
+        exit(1)
+    
     arg = sys.argv[1]
 
     if arg in functions:
