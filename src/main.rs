23d1a3d66112b04c935728834a256c10023a98df
Paul Mirkwood <paul.mirkwood@protonmail.com>
2024-05-24 23:42:57 +0200

    VGA cursor + start kernell shell

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use primoria::kprintln;
 pub extern "C" fn _start() -> ! {
     kprintln!("Hello World{}", "!");
 
+    for i in 0..10 {
+        kprintln!("n = {}", i);
+    }
+
     #[cfg(test)]
     test_main();
 
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,1 +1,2 @@
+pub mod kshell;
 pub mod ports;
